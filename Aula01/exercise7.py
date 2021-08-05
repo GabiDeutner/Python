67631f2b473f2e44c5272661c7f8d6f99d16232c
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-08-04 23:38:39 -0300

    Create exercise7.py

---
+++ Aula01/exercise7.py
@@ -0,0 +1,10 @@
+'''
+7. Elabore um programa que leia um valor e escrever a mensagem 
+É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+'''
+print('Digite um numero:')
+numero = float(input())
+if(numero>10):
+    print('É MAIOR QUE 10!')
+else:
+    print('NÃO É MAIOR QUE 10!')
