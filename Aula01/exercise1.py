c19addcd313caff58909fe0da79ecd44582f7d07
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-08-04 23:15:55 -0300

    Create exercise1.py

---
+++ Aula01/exercise1.py
@@ -0,0 +1,10 @@
+'''
+1. Elabore um programa que leia um número, e se ele for maior do que 20, escreva a metade desse número, caso contrário escreva o próprio número.
+'''
+
+print('Digite um numero:')
+numero = float(input())
+if(numero>20):
+    print(numero/2)
+else:
+    print(numero)
