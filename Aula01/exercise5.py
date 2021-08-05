84592ed6e71e12ce2fe192561a1de8b395a70b97
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-08-04 23:38:29 -0300

    Create exercise5.py

---
+++ Aula01/exercise5.py
@@ -0,0 +1,13 @@
+'''
+5. Faça um programa que receba dois números e apresente o menor valor
+'''
+print('Digite o primeiro numero:')
+numero1 = float(input())
+print('Digite o segundo numero:')
+numero2 = float(input())
+if(numero1>numero2):
+    print('o menor número é:')
+    print(numero2)
+else:
+    print('o menor número é:')
+    print(numero1)
