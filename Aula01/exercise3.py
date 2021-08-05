16eecd0b381243eef2d0083e9700a9af92a99018
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-08-04 23:38:16 -0300

    Create exercise3.py

---
+++ Aula01/exercise3.py
@@ -0,0 +1,10 @@
+'''
+3. Faça um programa para receber um valor e escrever se é “positivo”
+ ou “negativo” (considere o valor zero como positivo).
+'''
+print('Digite um numero:')
+numero = float(input())
+if(numero>=0):
+    print('Valor é positivo')
+else:
+    print('Valor é negativo')
