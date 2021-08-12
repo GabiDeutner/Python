08c6f429c88166f6754035da263f86dbb402d2c9
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-08-11 22:07:55 -0300

    Create exercise1.py

---
+++ Aula02/exercise1.py
@@ -0,0 +1,14 @@
+'''
+
+1. Escreva um programa para ler 2 valores (considere que não serão informados valores iguais) 
+e escrever o maior deles.
+
+'''
+print('Digite o numero 1:')
+numero1 = float(input())
+print('Digite o numero 2:')
+numero2 = float(input())
+if(numero1>numero2):
+    print(numero1)
+else:
+    print(numero2)
