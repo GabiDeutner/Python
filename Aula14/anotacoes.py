e8972ac83902320c44eee6f65c9bf09b5eb2d44f
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-11-10 21:37:44 -0300

    Create anotacoes.py

---
+++ Aula14/anotacoes.py
@@ -0,0 +1,70 @@
+'''
+# objetivo: imprimir numero entre 1 e 999
+
+c = int(input('digite a centena:'))
+d = int(input('digite a dezena: '))
+u = int(input('digite a unidade: '))
+
+if c==9
+   print('novecentos', end='')
+elif c==8:
+   print('oitocentos', end='')
+
+....
+elif c==1:
+   if d==0 and u ==0:
+      print('cem')
+   else:
+      print('cento e:')
+
+...
+
+if d==9:
+   print ('noventa ')
+...
+
+if d==1:
+   if u==9:
+      print ('dezenove ')
+
+...
+
+if d != 1:
+   if u==9:
+      print ('nove')
+
+
+
+
+
+
+#Criar uma Função de impressão
+
+def imprime_num(c,d,u):
+    colocar o código aqui
+
+cM = int(input('digite centena de milhão: '))
+dM = int(input('digite dezena de milhão '))
+uM = int(input('digite unidade de milhão: '))
+
+cm = int(input('digite centena de milhar: '))
+dm = int(input('digite dezena de milhar: '))
+um = int(input('digite unidade de milhar: '))
+
+c = int(input('digite centena: '))
+d = int(input('digite dezena: '))
+u = int(input('digite unidade: '))
+
+
+imprime_num(cM,dM,uM)
+#vai precisar de um if para ver se imprime milhoes ou milhao ou nada
+print('milhoes/milhao')
+
+imprime_num(cm,dm,um)
+print('mil')
+# if porque pode ser mil ou nada
+
+imprime_num(c,d,u)
+
+      
+'''
